2359fde6bd5501b252e82029776960e7b70371f6
Akira Sakakibara Ruiz <akira.sakakibararu@elorrieta-errekamari.com>
2024-12-19 17:55:35 +0100

    Commit final en la rama recuperacion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.akira_recuperacion"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.akira_recuperacion"
--- app/src/main/java/com/example/akira_recuperacion/MainActivity.kt
+++ app/src/main/java/com/example/akira_recuperacion/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.akira_recuperacion.ui.theme.Akira_RecuperacionTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +31,118 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Akira_RecuperacionTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                vistaRecuperacion()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun vistaRecuperacion() {
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Cyan)
+            .padding(40.dp),
+        verticalArrangement = Arrangement.Bottom
+    ){
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = Color.Black)
+                .padding(40.dp)
+        ){
+            Column {
+                texto("Hello Android!", Color.Red)
+            }
+            Column (
+                modifier = Modifier
+                    .weight(1f),
+                horizontalAlignment = Alignment.End
+            ){
+                texto("Hello Android!", Color.Cyan)
+            }
+        }
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = Color.Gray)
+                .padding(40.dp),
+
+        ){
+            Column (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentWidth(Alignment.CenterHorizontally),
+                verticalArrangement = Arrangement.Center
+            ){
+                Row { texto("Hello Android!", Color.Yellow) }
+                Row { texto("Hello Android!", Color.Red) }
+                Row { texto("Hello Android!", Color.White) }
+            }
+        }
+        Column (
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = Color.Green),
+                    horizontalAlignment = Alignment.End
+        ){
+            Row (
+                modifier = Modifier
+                    .background(color = Color.Blue)
+            ){
+                boton(1)
+                boton(2)
+                boton(3)
+            }
+            Row (
+                modifier = Modifier
+                    .background(color = Color.Yellow)
+                    .padding(horizontal =  40.dp)
+            ){
+                boton(1)
+                boton(2)
+                boton(3)
+            }
+            Row (
+                modifier = Modifier
+                    .background(color = Color.White)
+            ){
+                boton(1)
+                boton(2)
+                boton(3)
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Akira_RecuperacionTheme {
-        Greeting("Android")
+        vistaRecuperacion()
     }
+}
+
+@Composable
+fun boton (numero : Int) {
+    Button(
+        onClick = {},
+        modifier = Modifier
+            .size(60.dp),
+        shape = CircleShape
+    ) {
+        Text(
+            text = numero.toString()
+        )
+    }
+}
+
+@Composable
+fun texto (mensaje: String, colorElegido : Color) {
+    Text(
+        text = mensaje,
+        modifier = Modifier
+            .background(colorElegido)
+    )
 }
